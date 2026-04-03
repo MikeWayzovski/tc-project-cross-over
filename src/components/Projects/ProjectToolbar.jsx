0a4613cbdd3959d5783b9d097d97f63dc680e79d
Jackdemoel <jack_demoel@trimble.com>
2026-04-03 17:49:37 +0200

    Update ProjectToolbar.jsx

--- src/components/Projects/ProjectToolbar.jsx
+++ src/components/Projects/ProjectToolbar.jsx
@@ -53,7 +53,7 @@ const ProjectToolbar = ({
         <div className="view-toggle btn-group btn-group-sm me-3" role="group">
           <button 
             type="button" 
-            className={`btn btn-outline-secondary ${viewMode === 'grid' ? 'active' : ''}`} 
+            className={`btn btn-outline-secondary ${viewMode === 'squares-four' ? 'active' : ''}`} 
             onClick={() => setViewMode('grid')}
             title="Raster weergave"
           >
