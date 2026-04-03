3c7ce849b1afe56e766b48b779cf0be20e02096b
Jackdemoel <jack_demoel@trimble.com>
2026-04-03 08:56:54 +0200

    Added manifest
    adde manifest for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: true, // Dit lost het CORS probleem uit de Trimble documentatie op
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  }
 })
