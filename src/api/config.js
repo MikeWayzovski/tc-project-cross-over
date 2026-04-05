58ca937d76876b4a86e6fa3bdc89e026125b8a1d
Jackdemoel <jack_demoel@trimble.com>
2026-04-05 15:47:21 +0200

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -4,6 +4,7 @@ export const getBaseUrlForRegion = (regionName) => {
     case 'Europa': return 'https://app21.connect.trimble.com';
     case 'Azië': return 'https://app31.connect.trimble.com';
     case 'Australië': return 'https://app32.connect.trimble.com';
+    case 'northAmerica': return 'https://app.connect.trimble.com';
     default: return 'https://app.connect.trimble.com'; // Noord-Amerika is vaak de default
   }
 };
